383aa69dac043853eb2675f28a929f5e7ae74a0c
Dragoljub <zivanovic.dragoljub@gmail.com>
2024-08-30 13:25:42 +0200

    Init app

---
+++ src/App.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts"></script>
+
+<template>
+  <h1>Naslov</h1>
+  <div>
+    Lorem ipsum dolor sit amet consectetur adipisicing elit. Architecto deserunt impedit, ratione
+    dolores suscipit vel odio voluptatem repellendus autem sed, doloribus deleniti consequuntur
+    magni? Adipisci blanditiis voluptates sed ipsam provident?
+  </div>
+</template>
+
+<style scoped></style>
